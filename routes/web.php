819f0da3d4f0c460ffc6bad1cd4336ef00f8fb5d
aditya613 <adgupta006@gmail.com>
2026-01-28 22:44:12 +0530

    dashboard route add

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,12 @@ use App\Http\Controllers\Admin\JobController;
 use App\Http\Controllers\Admin\StudentController;
 use App\Http\Controllers\Admin\ApplicationController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
+    if (Auth::check()) {
+        return redirect()->route('admin.dashboard');
+    }
     return redirect()->route('login');
 });
 
